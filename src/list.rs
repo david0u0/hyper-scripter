9e932643b3e9e689ab22db1a82ef6118b2475a6b
why8ther <lturtsamuel@gmail.com>
2020-07-20 02:31:04 +0800

    列表顯示誰是最新腳本

--- src/list.rs
+++ src/list.rs
@@ -8,10 +8,7 @@ pub struct ListOptions {
     pub long: bool,
 }
 
-pub fn fmt_meta<W: Write>(w: &mut W, meta: &ScriptInfo, opt: &ListOptions) -> Result<bool> {
-    if !opt.show_hidden && meta.hidden {
-        return Ok(false);
-    }
+pub fn fmt_meta<W: Write>(w: &mut W, meta: &ScriptInfo, opt: &ListOptions) -> Result<()> {
     if opt.long {
         let exex_time = if let Some(t) = &meta.exec_time {
             t.to_string()
@@ -26,27 +23,43 @@ pub fn fmt_meta<W: Write>(w: &mut W, meta: &ScriptInfo, opt: &ListOptions) -> Re
     } else {
         write!(w, "{}\t{}", meta.ty, meta.name)?;
     }
-    Ok(true)
+    Ok(())
 }
 pub fn fmt_list<W: Write>(
     w: &mut W,
     scripts: impl IntoIterator<Item = ScriptInfo>,
     opt: &ListOptions,
 ) -> Result<()> {
-    let mut scripts: Vec<_> = scripts.into_iter().collect();
+    let mut scripts: Vec<_> = scripts
+        .into_iter()
+        .filter(|s| !s.hidden || opt.show_hidden)
+        .collect();
     scripts.sort_by_key(|m| m.name.clone());
+    let last_index = match scripts
+        .iter()
+        .enumerate()
+        .max_by_key(|(_, s)| s.last_time())
+    {
+        Some((i, _)) => i,
+        None => return Ok(()),
+    };
+
     let mut anonymous_printed = false;
     if opt.long {
         writeln!(w, "name\ttype\tlast edit time\tlast execute time")?;
     }
-    for meta in scripts.iter() {
+    for (i, meta) in scripts.iter().enumerate() {
         if let (false, ScriptName::Anonymous(_)) = (anonymous_printed, &meta.name) {
             anonymous_printed = true;
             writeln!(w, "--- Anonymous ---")?;
         }
-        if fmt_meta(w, meta, opt)? {
-            write!(w, "\n")?;
+        if i == last_index {
+            write!(w, " *")?;
+        } else {
+            write!(w, "  ")?;
         }
+        fmt_meta(w, meta, opt)?;
+        write!(w, "\n")?;
     }
     Ok(())
 }

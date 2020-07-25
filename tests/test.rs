ed2514bf09d471b1b9f8c3579b51b87eee6a4a41
why8ther <lturtsamuel@gmail.com>
2020-07-25 20:13:51 +0800

    改用鎖來確保單線程測試

--- tests/test.rs
+++ tests/test.rs
@@ -1,12 +1,17 @@
 use instant_scripter::path;
 use std::io::{BufRead, BufReader};
-use std::path::PathBuf;
 use std::process::{Command, Stdio};
+use std::sync::{Mutex, MutexGuard};
 
-fn setup(name: &str) -> Env {
+lazy_static::lazy_static! {
+    static ref LOCK: Mutex<()> = Mutex::new(());
+}
+
+fn setup<'a>() -> MutexGuard<'a, ()> {
+    let guard = LOCK.lock().unwrap();
     let _ = env_logger::try_init();
-    let p = path::get_sys_path().unwrap().join(format!("test_{}", name));
-    match std::fs::remove_dir_all(&p) {
+    path::set_path_from_sys().unwrap();
+    match std::fs::remove_dir_all(path::get_path()) {
         Ok(_) => (),
         Err(e) => {
             if e.kind() != std::io::ErrorKind::NotFound {
@@ -14,35 +19,30 @@ fn setup(name: &str) -> Env {
             }
         }
     }
-    Env(p)
+    guard
 }
-struct Env(PathBuf);
-impl Env {
-    pub fn check_exist(&self, path: &str) -> bool {
-        self.0.join(path).exists()
-    }
-    pub fn run(&self, args: &[&str]) -> Result<String, i32> {
-        let mut cmd = Command::new("./target/debug/instant_scripter");
-        let mut full_args = vec!["-p", self.0.to_str().unwrap()];
-        full_args.extend(args);
-        let mut child = cmd.args(full_args).stdout(Stdio::piped()).spawn().unwrap();
-        let stdout = child.stdout.as_mut().unwrap();
-        let mut out_str = vec![];
-        let reader = BufReader::new(stdout);
-        reader
-            .lines()
-            .filter_map(|line| line.ok())
-            .for_each(|line| {
-                println!("{}", line);
-                out_str.push(line);
-            });
+fn check_exist(p: &str) -> bool {
+    path::get_path().join(p).exists()
+}
+fn run(args: &[&str]) -> Result<String, i32> {
+    let mut cmd = Command::new("./target/debug/instant_scripter");
+    let mut child = cmd.args(args).stdout(Stdio::piped()).spawn().unwrap();
+    let stdout = child.stdout.as_mut().unwrap();
+    let mut out_str = vec![];
+    let reader = BufReader::new(stdout);
+    reader
+        .lines()
+        .filter_map(|line| line.ok())
+        .for_each(|line| {
+            println!("{}", line);
+            out_str.push(line);
+        });
 
-        let status = child.wait().unwrap();
-        if status.success() {
-            Ok(out_str.join("\n"))
-        } else {
-            Err(status.code().unwrap_or_default())
-        }
+    let status = child.wait().unwrap();
+    if status.success() {
+        Ok(out_str.join("\n"))
+    } else {
+        Err(status.code().unwrap_or_default())
     }
 }
 
@@ -50,11 +50,11 @@ const MSG: &'static str = "你好，腳本管理員！";
 const MSG_JS: &'static str = "你好，腳本管理員！.js";
 #[test]
 fn test_tags() {
-    let r = setup("tags");
-    r.run(&["e", "-c", &format!("echo \"{}\"", MSG)]).unwrap();
-    assert_eq!(MSG, r.run(&["-"]).unwrap());
+    let _g = setup();
+    run(&["e", "-c", &format!("echo \"{}\"", MSG)]).unwrap();
+    assert_eq!(MSG, run(&["-"]).unwrap());
 
-    r.run(&[
+    run(&[
         "-t",
         "super_tag,hide",
         "e",
@@ -65,43 +65,40 @@ fn test_tags() {
         &format!("console.log(\"{}\")", MSG_JS),
     ])
     .unwrap();
-    r.run(&["tesjs"])
-        .expect_err("標籤沒有篩選掉不該出現的腳本！");
-    assert_eq!(MSG_JS, r.run(&["-t", "super_tag", "-"]).unwrap());
+    run(&["tesjs"]).expect_err("標籤沒有篩選掉不該出現的腳本！");
+    assert_eq!(MSG_JS, run(&["-t", "super_tag", "-"]).unwrap());
 
-    assert_eq!(MSG, r.run(&[".1"]).expect("標籤篩選把舊的腳本搞爛了！"));
+    assert_eq!(MSG, run(&[".1"]).expect("標籤篩選把舊的腳本搞爛了！"));
 
-    r.run(&["tesjs"])
-        .expect_err("標籤沒有篩選掉不該出現的腳本！可能是上上個操作把設定檔寫爛了");
-    r.run(&["tags", "all"]).unwrap();
-    r.run(&["tesjs"]).expect("沒吃到設定檔的標籤？");
+    run(&["tesjs"]).expect_err("標籤沒有篩選掉不該出現的腳本！可能是上上個操作把設定檔寫爛了");
+    run(&["tags", "all"]).unwrap();
+    run(&["tesjs"]).expect("沒吃到設定檔的標籤？");
 }
 
 #[test]
 fn test_mv() {
-    let r = setup("mv");
-    r.run(&["e", "-x", "js", "-c", &format!("echo \"{}\"", MSG)])
-        .unwrap();
-    r.run(&["-"]).expect_err("用 nodejs 執行 echo ……？");
+    let _g = setup();
+    run(&["e", "-x", "js", "-c", &format!("echo \"{}\"", MSG)]).unwrap();
+    run(&["-"]).expect_err("用 nodejs 執行 echo ……？");
 
-    r.run(&["mv", "1", "-x", "sh"]).unwrap();
-    assert_eq!(MSG, r.run(&["-"]).unwrap());
-    assert!(r.check_exist(".anonymous/1.sh"), "改腳本類型失敗");
+    run(&["mv", "1", "-x", "sh"]).unwrap();
+    assert_eq!(MSG, run(&["-"]).unwrap());
+    assert!(check_exist(".anonymous/1.sh"), "改腳本類型失敗");
     assert!(
-        !r.check_exist(".anonymous/1.js"),
+        !check_exist(".anonymous/1.js"),
         "改了腳本類型舊檔案還留著？"
     );
 
-    r.run(&["mv", "1", "-t", "hide"]).unwrap();
-    r.run(&["-"]).expect_err("用 mv 修改標籤失敗？");
+    run(&["mv", "1", "-t", "hide"]).unwrap();
+    run(&["-"]).expect_err("用 mv 修改標籤失敗？");
 }
 
 const TALKER: &'static str = "--腳本小子";
 const APPEND: &'static str = "第二行";
 #[test]
 fn test_args() {
-    let r = setup("args");
-    r.run(&[
+    let _g = setup();
+    run(&[
         "e",
         "test-with-args",
         "-c",
@@ -110,40 +107,35 @@ fn test_args() {
     .unwrap();
     assert_eq!(
         format!("{}：{}\n{}", TALKER, MSG, APPEND),
-        r.run(&["-", TALKER, APPEND]).unwrap()
+        run(&["-", TALKER, APPEND]).unwrap()
     );
 }
 
 #[test]
 fn test_exact() {
-    let r = setup("exact");
-    r.run(&["e", "test-exact", "-c", "echo 'test exact!'"])
-        .unwrap();
-    r.run(&["tesct"]).expect("模糊搜不到東西！");
-    r.run(&["=tesct"]).expect_err("打錯名字卻還搜得到！");
-    r.run(&["=test-exact"]).expect("打完整名字卻搜不到！");
+    let _g = setup();
+    run(&["e", "test-exact", "-c", "echo 'test exact!'"]).unwrap();
+    run(&["tesct"]).expect("模糊搜不到東西！");
+    run(&["=tesct"]).expect_err("打錯名字卻還搜得到！");
+    run(&["=test-exact"]).expect("打完整名字卻搜不到！");
 }
 
 #[test]
 fn test_prev() {
-    let r = setup("prev");
-    r.run(&["e", "test-prev1", "-c", "echo 'test prev 1'"])
-        .unwrap();
-    r.run(&["e", "test-prev2", "-c", "echo 'test prev 2'"])
-        .unwrap();
-    r.run(&["e", "test-prev3", "-c", "echo 'test prev 3'"])
-        .unwrap();
+    let _g = setup();
+    run(&["e", "test-prev1", "-c", "echo 'test prev 1'"]).unwrap();
+    run(&["e", "test-prev2", "-c", "echo 'test prev 2'"]).unwrap();
+    run(&["e", "test-prev3", "-c", "echo 'test prev 3'"]).unwrap();
 
-    assert_eq!(r.run(&["^2"]).unwrap(), "test prev 2");
-    assert_eq!(r.run(&["^2"]).unwrap(), "test prev 3");
-    assert_eq!(r.run(&["^^^"]).unwrap(), "test prev 1");
-    assert_eq!(r.run(&["cat", "^2"]).unwrap(), "echo 'test prev 3'");
+    assert_eq!(run(&["^2"]).unwrap(), "test prev 2");
+    assert_eq!(run(&["^2"]).unwrap(), "test prev 3");
+    assert_eq!(run(&["^^^"]).unwrap(), "test prev 1");
+    assert_eq!(run(&["cat", "^2"]).unwrap(), "echo 'test prev 3'");
     assert_eq!(
-        r.run(&["-"]).unwrap(),
+        run(&["-"]).unwrap(),
         "test prev 3",
         "cat 沒有確實影響到腳本時序"
     );
 
-    r.run(&["^^^^"])
-        .expect_err("明明只有三個腳本，跟我說有第四新的？");
+    run(&["^^^^"]).expect_err("明明只有三個腳本，跟我說有第四新的？");
 }

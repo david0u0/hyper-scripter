5f87862b5def2353617be93ac7d49b0b82cf6c93
why8ther <lturtsamuel@gmail.com>
2021-03-28 00:14:47 +0800

    補上刪除（忽略）歷史事件的測試

--- hyper-scripter/tests/history.rs
+++ hyper-scripter/tests/history.rs
@@ -49,3 +49,37 @@ fn test_history_args_order() {
     let recorded = run("history show receiver --offset 2 --limit 999").unwrap();
     assert_list(&recorded, &["first"]);
 }
+
+#[test]
+fn test_history_args_rm() {
+    let _g = setup();
+    run("e arg-receiver | echo $@").unwrap();
+
+    run("receiver third").unwrap();
+    run("receiver second").unwrap();
+    run("receiver first").unwrap();
+    run("receiver third").unwrap();
+    run("receiver second").unwrap();
+    run("receiver first").unwrap();
+    run("receiver third").unwrap();
+    run("receiver second").unwrap();
+    run("receiver first").unwrap();
+
+    let recorded = run("history show receiver").unwrap();
+    assert_list(&recorded, &["first", "second", "third"]);
+    let recorded = run("history show receiver --offset 1 --limit 1").unwrap();
+    assert_list(&recorded, &["second"]);
+    let recorded = run("history show receiver --offset 2 --limit 999").unwrap();
+    assert_list(&recorded, &["third"]);
+
+    run("history rm receiver 1").unwrap(); // 幹掉 "first"
+
+    let recorded = run("history show receiver").unwrap();
+    assert_list(&recorded, &["second", "third"]);
+
+    assert_eq!(run("run -p -").unwrap(), "second", "沒有刪成功？");
+
+    run("history rm receiver 2").unwrap(); // 幹掉 "third"
+    let recorded = run("history show receiver").unwrap();
+    assert_list(&recorded, &["second"]);
+}

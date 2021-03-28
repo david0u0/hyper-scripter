7873e9c525caa56d1f713e89b99e192d51c35f9a
why8ther <lturtsamuel@gmail.com>
2021-03-28 12:17:48 +0800

    可以在史學家中刪除歷史事件了 但 offset 尚未處理…

--- hyper-scripter-util/util/historian.rb
+++ hyper-scripter-util/util/historian.rb
@@ -1,47 +1,58 @@
-# [HS_HELP]: Interactively run script from to history.
+# [HS_HELP]: Interactively run script from history.
 # [HS_HELP]:
 # [HS_HELP]: e.g.:
 # [HS_HELP]:     hs historian -f hs hs/test --limit 20
 
 require_relative './common.rb'
 
-history = HS_ENV.do_hs("history show #{ARGV.join(' ')}", false)
-exit 1 unless $?.success?
-lines = history.lines.map{ |s|  s.length > 0 ? s : " " }
-name = lines.slice!(0).strip
+lines = []
+lines_count = 0
+name = ''
 
-if lines.length == 0 then
-  puts "history is empty"
-  exit 0
+load_history = lambda do |args|
+  history = HS_ENV.do_hs("history show #{args}", false)
+  exit 1 unless $?.success?
+  lines = history.lines.map { |s| s.length > 0 ? s : ' ' }
+  name = lines.slice!(0).strip
+
+  if lines.length == 0
+    puts 'history is empty'
+    exit 0
+  end
+  lines_count = lines.length
 end
 
-lines_count = lines.length
+load_history.call(ARGV.join(' '))
+
 pos = 0
 selected = false
 
 loop do
+  reload = false
   lines.each_with_index do |line, i|
     leading = i == pos ? '>' : ' '
-    $stderr.print "#{leading} #{i}. #{line}"
+    $stderr.print "#{leading} #{i + 1}. #{line}"
   end
 
   begin
-    system("stty raw -echo")
+    system('stty raw -echo')
     resp = $stdin.getc
 
     case resp
-    when ?q, ?Q
+    when 'q', 'Q'
       break
-    when ?j, ?J
+    when 'j', 'J'
       pos = (pos + 1) % lines_count
-    when ?k, ?K
+    when 'k', 'K'
       pos = (pos - 1 + lines_count) % lines_count
-    when ?\r
+    when "\r"
       selected = true
       break
+    when 'd', 'D'
+      reload = true
     end
   ensure
-    system("stty -raw echo")
+    system('stty -raw echo')
   end
 
   $stdout.flush
@@ -50,11 +61,16 @@ loop do
     $stderr.print "\e[A"
   end
   $stderr.print "\r\e[J"
+
+  if reload
+    HS_ENV.do_hs("history rm =#{name}! #{pos + 1}", false)
+    load_history.call("=#{name}!")
+  end
 end
 
 $stderr.print "\e[#{lines_count}E"
 
-if selected then
+if selected
   cmd = "=#{name}! #{lines[pos]}"
   puts cmd
   history = HS_ENV.exec_hs(cmd, false)

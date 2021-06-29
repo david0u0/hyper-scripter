c4b2b62f1ed5bd0f7561551db565492eb4f47f1a
why8ther <lturtsamuel@gmail.com>
2021-06-29 01:03:51 +0000

    對近期事件的 script_id 建立主鍵

--- hyper-scripter/migrations/202106131448_last_event.sql
+++ hyper-scripter/migrations/202106131448_last_event.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS last_events (
-    script_id integer NOT NULL,
+    script_id integer PRIMARY KEY NOT NULL,
     last_time datetime,
     write datetime,
     read datetime,

996986890881e8bbfad1c075c503aa85849976ba
why8ther <lturtsamuel@gmail.com>
2021-07-25 07:28:41 +0000

    開啟資料庫失敗時，會提高 journal mode 嘗試再開一次

--- hyper-scripter/src/db.rs
+++ hyper-scripter/src/db.rs
@@ -7,22 +7,26 @@ use sqlx::{
 /// 有可能改變 need_journal 的值。若為初始化，不論如何都使用 journal
 pub async fn get_pool(need_journal: &mut bool) -> Result<(SqlitePool, bool)> {
     let file = crate::path::get_home().join(".script_info.db");
+    if !file.exists() {
+        *need_journal = true;
+        let pool = crate::migration::do_migrate(file).await?;
+        return Ok((pool, true));
+    }
+
     let mut opt = SqliteConnectOptions::new().filename(&file);
     if !*need_journal {
         opt = opt.journal_mode(SqliteJournalMode::Off);
     }
     let res = SqlitePool::connect_with(opt).await;
-    let init: bool;
     let pool = match res {
-        Err(_) => {
-            init = true;
+        Err(err) => {
+            // 通常是有其它程序用 journal mode 鎖住資料庫，例如正在編輯另一個腳本
+            log::warn!("資料庫錯誤 {}，嘗試用 journal 再開一次", err);
             *need_journal = true;
-            crate::migration::do_migrate(file).await?
+            let opt = SqliteConnectOptions::new().filename(&file);
+            SqlitePool::connect_with(opt).await?
         }
-        Ok(pool) => {
-            init = false;
-            pool
-        }
+        Ok(pool) => pool,
     };
-    Ok((pool, init))
+    Ok((pool, false))
 }

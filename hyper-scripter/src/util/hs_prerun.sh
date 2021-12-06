651e4c17017d7d884870cbc5cd7a1c3731633f7e
why8ther <lturtsamuel@gmail.com>
2021-12-06 09:50:58 +0000

    修正預處理腳本，使其能處理不見容於命令行的字元 例如單引號 => [HS_ENV_HELP]: VAR this shouldn't fail...

--- hyper-scripter/src/util/hs_prerun.sh
+++ hyper-scripter/src/util/hs_prerun.sh
@@ -7,6 +7,7 @@ RED='\033[1;31m'
 BLUE='\033[1;34m'
 CYAN='\033[1;36m'
 GREEN='\033[1;32m'
+WHITE='\033[1;37m'
 NC='\033[0m'
 
 echo "$HS_ENV_HELP" | while read line
@@ -17,14 +18,14 @@ do
 
     ENV=$(cut -d ' ' -f 1 <<< "$line")
     MSG=$(cut -d ' ' -f 2- <<< "$line ")
-    MSG=$(echo $MSG | xargs)
+    MSG=$(echo $MSG | tr -s " ")
     if [[ -v $ENV ]]; then
         VAR=$RED${!ENV}$NC
     else
-        VAR=--
+        VAR=$WHITE--$NC
     fi
     if [ ! "$MSG" = "" ]; then
         MSG=" ($MSG)"
     fi
-    >&2 echo -e ${CYAN}$ENV${NC} = $VAR$MSG
+    >&2 echo -e ${CYAN}$ENV:${NC} $VAR$MSG
 done
